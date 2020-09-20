128fbba2fe4055c2ee1ebf931f28bd88af8fffdc
Herve Loiret <herve.loiret@gmail.com>
2020-09-20 23:24:28 +0200

    first commit

---
+++ src/main/java/fr/eurler/invoicemanager/controller/InvoiceController.java
@@ -0,0 +1,20 @@
+package fr.eurler.invoicemanager.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class InvoiceController {
+    
+    public void addInvoice() {
+
+    }
+
+    public void getOneInvoice() {
+
+    }
+
+    public void getInvoices() {
+
+    }
+
+}
---
+++ src/test/java/fr/eurler/invoicemanager/InvoicemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eurler.invoicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
